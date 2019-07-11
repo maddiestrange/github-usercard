e6b4443f8cb7afa586f7481443cad31ca644bb7f
maddiestrange <maddie.strange@gmail.com>
2019-07-11 16:20:47 -0500

    initial step 3 build-out

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -60,3 +60,44 @@ const followersArray = [];
   luishrd
   bigknell
 */
+function createCards(data){
+  const card = document.createElement('div')
+  const img = document.createElement('img')
+  const cardInfo = document.createElement('div')
+  const name = document.createElement('h3')
+  const username = document.createElement('p')
+  const location = document.createElement('p')
+  const profile = document.createElement('p')
+  const address = document.createElement('a')
+  const followers = document.createElement('p')
+  const following = document.createElement('p')
+  const bio = document.createElement('p')
+
+  card.appendChild(img)
+  card.appendChild(cardInfo)
+  cardInfo.appendChild(name)
+  cardInfo.appendChild(username)
+  cardInfo.appendChild(location)
+  cardInfo.appendChild(profile)
+  profile.appendChild(address)
+  cardInfo.appendChild(followers)
+  cardInfo.appendChild(following)
+  cardInfo.appendChild(bio)
+
+  card.classList.add('card')
+  cardInfo.classList.add('card-info')
+  name.classList.add('name')
+  username.classList.add('username')
+
+  img.src = data.avatar_url
+  name.textContent = data.name
+  username.textContent = data.login
+  location.textContent = `Location ${data.location}`
+  profile.textContent = 'Profile:'
+  address.href = data.url
+  followers.textContent = data.followers
+  followers.textContent = data.following
+  bio.textContent = `Bio: ${data.bio}`
+
+
+}
